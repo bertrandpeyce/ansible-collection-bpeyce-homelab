90cca64396ebb44cb2859a2fa4316eb2bfa234e4
Bertrand Peyce <bertrand.peyce@gmail.com>
2025-08-14 15:18:07 +0200

    + test kvm + makefile postfix + makefile test global

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 #TODO: take version into consideration
 
+.PHONY: all build install clean test
+
 all: build install clean
 
 build:
 	ansible-galaxy collection build . --force
+
 install:
 	ansible-galaxy collection install bpeyce-homelab-1.0.0.tar.gz -p $(HOME)/.ansible/collections --force
+
 clean:
 	rm bpeyce-homelab-1.0.0.tar.gz
+
+test: build install clean
+	$(MAKE) -C $(CURDIR)/roles test
--- roles/kvm_host/tests/test.py
+++ roles/kvm_host/tests/test.py
@@ -1,8 +1,13 @@
-
-kvm_host_ubuntu_packages = [ "bridge-utils", "libvirt-clients", "libvirt-daemon-system",  "virt-manager"] 
+kvm_host_ubuntu_packages = [
+    "bridge-utils",
+    "libvirt-clients",
+    "libvirt-daemon-system",
+    "virt-manager",
+]
 # qemu-kvm is a virtual package and cant be checked
 
-kvm_host_ubuntu_services = ["libvirtd]
+kvm_host_ubuntu_services = ["libvirtd"]
+
 
 def test_installed_packages(host):
     match host.system_info.distribution:
@@ -10,7 +15,8 @@ def test_installed_packages(host):
             for package in kvm_host_ubuntu_packages:
                 pkg = host.package(package)
                 assert pkg.is_installed
-                #assert pkg.version.startswith("1:")
+                # assert pkg.version.startswith("1:")
+
 
 def test_running_services(host):
     match host.system_info.distribution:
@@ -21,3 +27,8 @@ def test_running_services(host):
                 assert svc.is_enabled
 
 
+def test_vagrant_user_in_libvirt_group(host):
+    vagrant_user = host.user("vagrant")
+    groups = vagrant_user.groups
+    assert "kvm" in groups
+    assert "libvirt" in groups
